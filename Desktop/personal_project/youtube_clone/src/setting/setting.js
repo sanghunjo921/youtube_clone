79b2967323c0a8a9f07d03856a7f4a040347c0da
Sanghun Jo <sanghunjo@Sanghunui-MacBookPro.local>
2023-04-11 12:21:44 +0900

    youtube service

--- Desktop/personal_project/youtube_clone/src/setting/setting.js
+++
@@ -1,7 +0,0 @@
-export const commonSetting = {
-    title: "Youtube",
-    apiKey: process.env.REACT_APP_YOUTUBE_API ?? "AIzaSyCqB7A4K2OeNmnl_7KPWS6-4iE2gD_7r6E",
-    baseUrl: "https://youtube.googleapis.com/youtube/v3",
-    isProduct: true,
-}
-
